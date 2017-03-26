c11ed0cc5e5c7330d987095b32311331bfff001f
Rawan Moukalled <rawan95@gmail.com>
2017-03-26 21:12:37 +0300

    Cursor can be moved, setup to go to next pages

--- airstrikegame.ino
+++ airstrikegame.ino
@@ -36,83 +36,126 @@
 #include <Terminal8e.h>
 Screen_HX8353E screen;
 
-
 #define ROWS 80 // max 80
 #define COLS 60 // max 60
 
 #define ALIVE   0x08
 #define DEAD    0x00
 
-
-#define NewGame 33
-#define Load Game 32
-
-#define joystickX 2
-#define joystickY 26
-
+#define joystickX 2 //pin for the joystick X coordinates
+#define joystickY 26 //pin for the joystick Y coordinates
+#define Enter 32 //pin for the Push button
 
 // Define variables and constants
-uint8_t _screen[2][ROWS][COLS];
-uint32_t chrono;
-uint8_t page;
-uint16_t colours[16];
-uint16_t generation;
-uint8_t rowSize, colSize;
-uint8_t i, j;
-uint16_t x, y, x00, y00; //joystick location
+uint16_t jsX, jsY, arrowX, arrowY; //current positions of the joystick and arrow 
 
-enum mode_t {
-  SELECTGAME, GAME, PAUSE, LOADGAME
-  }, 
-  mode = SELECTGAME;
+// Which mode of the game we are at
+enum screen_mode_t {
+  SELECTGAMETYPE, GAME, PAUSE, LOADGAME
+} mode = SELECTGAMETYPE; // main mode to be used 
 
+// Display Airstrike Game 
 void Initialize_Screen(){
-    screen.clear(cyanColour);
-    screen.setFontSolid(true);
-    screen.setFontSize(1);
-    screen.gText(13, 50, "AirStrike Game");
+  screen.clear(cyanColour);
+  screen.setFontSolid(true);
+  screen.setFontSize(1);
+  screen.gText(13, 50, "AirStrike Game");
 }
 
-void Select_Game_Option(){
+//Select between a new game or loading one 
+void Select_Game_Type(){
   screen.clear(blackColour);
   screen.setFontSolid(false);
   screen.setFontSize(1);
-  screen.gText(3, 50, ">");
+  arrowX = 3;
+  arrowY = 50;
+  screen.gText(arrowX, arrowY, ">");
   screen.gText(15, 50, "New Game");
   screen.gText(15, 70, "Load Game");
 }
 
-void Arrow_Move(){
-  x = analogRead(joystickX);
-  y = analogRead(joystickY);
-  
+//checks the joystick movements
+void Read_Joystick() {
+  //convert from input to output range, Y goes from 0 to 128 top to bottom of the screen
+  jsX = map(analogRead(joystickX), 0, 4096, 0, 128);
+  jsY = map(analogRead(joystickY), 0, 4096, 128, 0);
+  //joystick was moved down
+  if( jsY > 90) {
+    Place_Arrow(arrowX, arrowY + 20);
+  } else if (jsY < 30) { //joystick up
+    Place_Arrow(arrowX, arrowY - 20);
+  }
+}
+
+// check for the push button being pressed
+void Read_Enter() {
+  int enterState = digitalRead(Enter);
+  //button has been pushed
+  if(enterState == LOW) {
+    if(mode == SELECTGAMETYPE){
+      Display_New_Page(arrowY);
+    }
+  }
+}
+
+void Display_New_Page(uint16_t y) {
+  if(mode == SELECTGAMETYPE){
+    if(y == 50){
+      //Display_New_Game();
+    }else if(y == 70) {
+      //Display_Load_Game();
+    }
+  }
+}
+
+void Place_Arrow(uint16_t x, uint16_t y){
+    uint16_t minY, maxY;
+    if(mode == SELECTGAMETYPE) {
+      minY = 50; 
+      maxY = 70;
+    }
+
+    //erase current arrow by placing a full black box on it
+    screen.setPenSolid(true);
+    screen.dRectangle(arrowX, arrowY, 9, 10, blackColour);
+
+    //loop to bottom or top if the next y position exceeds the 
+    //current menu items position range
+    if( y > maxY) {
+      arrowY = minY;
+    } else if (y < minY) {
+      arrowY = maxY;
+    } else{ //update arrow position 
+      arrowX = x;
+      arrowY = y;  
+    }
+    screen.gText(arrowX, arrowY, ">");
 }
 
 // Add setup code
 void setup() {
-    Serial.begin(9600); //UART with its baudrate
-    delay(100);
+  Serial.begin(9600); //UART with its baudrate
+  delay(100);
+  pinMode(Enter, INPUT_PULLUP);
     
-    screen.begin();
-    screen.setOrientation(0);
-    joystickX00 = 0;
-    joystickY00 = 0;
-
-    Initialize_Screen();  //Print first the message: "Airstrike Game"
-    delay(2000);
+  screen.begin();
+  screen.setOrientation(0);
     
-    screen.clear();   //Go to the next page
+  Initialize_Screen();  //Print first the message: "Airstrike Game"
+  delay(2000);
     
-    Select_Game_Option();   //User can choose between a new game or to load a saved game
-
+  screen.clear();   //Go to the next page
+    
+  Select_Game_Type();   //User can choose between a new game or to load a saved game  
 }
 
 // Add loop code
 void loop() {
-    if(mode == SELECTGAME){
-      Arrow_Move();
-    }
-    
+  if(mode == SELECTGAMETYPE){
+    Read_Joystick();
+    Read_Enter();
+    delay(280);  
+  } 
 }
 
 
